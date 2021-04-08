b19cfbda841d0d538115aa5b4ef20d47e438ae9a
Joakim Repomaa <mail@j.repomaa.com>
2021-04-08 13:20:15 +0200

    add CryptoKey to global scope
    some libraries use instanceof checks that require CryptoKey in global scope

--- src/evaluate-script.ts
+++ src/evaluate-script.ts
@@ -1,6 +1,13 @@
-import { EventTarget } from 'event-target-shim';
-import vm from 'vm';
-import { GraphQLEvent, GraphQLEventWithParent, GraphQLEventFields, ResolverResponse, AuthHeaderField, WebHookGraphQLEvent } from '@slash-graphql/lambda-types'
+import { EventTarget } from "event-target-shim";
+import vm from "vm";
+import {
+  GraphQLEvent,
+  GraphQLEventWithParent,
+  GraphQLEventFields,
+  ResolverResponse,
+  AuthHeaderField,
+  WebHookGraphQLEvent,
+} from "@slash-graphql/lambda-types";
 
 import fetch, { Request, Response, Headers } from "node-fetch";
 import { URL } from "url";
@@ -8,48 +15,57 @@ import atob from "atob";
 import btoa from "btoa";
 import { TextDecoder, TextEncoder } from "util";
 import { Crypto } from "node-webcrypto-ossl";
-import { graphql, dql } from './dgraph';
+import { CryptoKey } from "webcrypto-core";
+import { graphql, dql } from "./dgraph";
 
-function getParents(e: GraphQLEventFields): (Record<string,any>|null)[] {
-  return e.parents || [null]
+function getParents(e: GraphQLEventFields): (Record<string, any> | null)[] {
+  return e.parents || [null];
 }
 
 class GraphQLResolverEventTarget extends EventTarget {
-  addMultiParentGraphQLResolvers(resolvers: {[key: string]: (e: GraphQLEvent) => ResolverResponse}) {
+  addMultiParentGraphQLResolvers(resolvers: {
+    [key: string]: (e: GraphQLEvent) => ResolverResponse;
+  }) {
     for (const [name, resolver] of Object.entries(resolvers)) {
-      this.addEventListener(name, e => {
-        const event = e as unknown as GraphQLEvent;
-        event.respondWith(resolver(event))
-      })
+      this.addEventListener(name, (e) => {
+        const event = (e as unknown) as GraphQLEvent;
+        event.respondWith(resolver(event));
+      });
     }
   }
 
-  addGraphQLResolvers(resolvers: { [key: string]: (e: GraphQLEventWithParent) => (any | Promise<any>) }) {
+  addGraphQLResolvers(resolvers: {
+    [key: string]: (e: GraphQLEventWithParent) => any | Promise<any>;
+  }) {
     for (const [name, resolver] of Object.entries(resolvers)) {
-      this.addEventListener(name, e => {
-        const event = e as unknown as GraphQLEvent;
-        event.respondWith(getParents(event).map(parent => resolver({...event, parent})))
-      })
+      this.addEventListener(name, (e) => {
+        const event = (e as unknown) as GraphQLEvent;
+        event.respondWith(
+          getParents(event).map((parent) => resolver({ ...event, parent }))
+        );
+      });
     }
   }
 
-  addWebHookResolvers(resolvers: { [key: string]: (e: WebHookGraphQLEvent) => (any | Promise<any>) }) {
+  addWebHookResolvers(resolvers: {
+    [key: string]: (e: WebHookGraphQLEvent) => any | Promise<any>;
+  }) {
     for (const [name, resolver] of Object.entries(resolvers)) {
-      this.addEventListener(name, e => {
-        const event = e as unknown as WebHookGraphQLEvent;
-        event.respondWith(resolver(event))
-      })
+      this.addEventListener(name, (e) => {
+        const event = (e as unknown) as WebHookGraphQLEvent;
+        event.respondWith(resolver(event));
+      });
     }
   }
 }
 
 function newContext(eventTarget: GraphQLResolverEventTarget) {
-  const env = {} as Record<string, string | undefined>
+  const env = {} as Record<string, string | undefined>;
   Object.entries(process.env).forEach(([key, value]) => {
-    if (key.startsWith('EXPOSED_')) {
-      env[key] = value
+    if (key.startsWith("EXPOSED_")) {
+      env[key] = value;
     }
-  })
+  });
 
   return vm.createContext({
     // From fetch
@@ -70,6 +86,7 @@ function newContext(eventTarget: GraphQLResolverEventTarget) {
     crypto: new Crypto(),
     TextDecoder,
     TextEncoder,
+    CryptoKey,
 
     // Debugging
     console,
@@ -84,7 +101,9 @@ function newContext(eventTarget: GraphQLResolverEventTarget) {
     self: eventTarget,
     addEventListener: eventTarget.addEventListener.bind(eventTarget),
     removeEventListener: eventTarget.removeEventListener.bind(eventTarget),
-    addMultiParentGraphQLResolvers: eventTarget.addMultiParentGraphQLResolvers.bind(eventTarget),
+    addMultiParentGraphQLResolvers: eventTarget.addMultiParentGraphQLResolvers.bind(
+      eventTarget
+    ),
     addGraphQLResolvers: eventTarget.addGraphQLResolvers.bind(eventTarget),
     addWebHookResolvers: eventTarget.addWebHookResolvers.bind(eventTarget),
 
@@ -94,35 +113,48 @@ function newContext(eventTarget: GraphQLResolverEventTarget) {
 }
 
 export function evaluateScript(source: string) {
-  const script = new vm.Script(source)
+  const script = new vm.Script(source);
   const target = new GraphQLResolverEventTarget();
-  const context = newContext(target)
+  const context = newContext(target);
   script.runInContext(context);
 
-  return async function(e: GraphQLEventFields): Promise<any | undefined> {
+  return async function (e: GraphQLEventFields): Promise<any | undefined> {
     let retPromise: ResolverResponse | undefined = undefined;
     const event = {
       ...e,
-      respondWith: (x: ResolverResponse) => { retPromise = x },
-      graphql: (query: string, variables: Record<string, any>, ah?: AuthHeaderField) => graphql(query, variables, ah || e.authHeader),
+      respondWith: (x: ResolverResponse) => {
+        retPromise = x;
+      },
+      graphql: (
+        query: string,
+        variables: Record<string, any>,
+        ah?: AuthHeaderField
+      ) => graphql(query, variables, ah || e.authHeader),
       dql,
+    };
+    if (e.type === "$webhook" && e.event) {
+      event.type = `${e.event?.__typename}.${e.event?.operation}`;
     }
-    if (e.type === '$webhook' && e.event) {
-      event.type = `${e.event?.__typename}.${e.event?.operation}` 
-    }
-    target.dispatchEvent(event)
+    target.dispatchEvent(event);
 
-    if(retPromise === undefined) {
-      return undefined
+    if (retPromise === undefined) {
+      return undefined;
     }
 
     const resolvedArray = await (retPromise as ResolverResponse);
-    if(!Array.isArray(resolvedArray) || resolvedArray.length !== getParents(e).length) {
-      process.env.NODE_ENV != "test" && e.type !== '$webhook' && console.error(`Value returned from ${e.type} was not an array or of incorrect length`)
-      return undefined
+    if (
+      !Array.isArray(resolvedArray) ||
+      resolvedArray.length !== getParents(e).length
+    ) {
+      process.env.NODE_ENV != "test" &&
+        e.type !== "$webhook" &&
+        console.error(
+          `Value returned from ${e.type} was not an array or of incorrect length`
+        );
+      return undefined;
     }
 
     const response = await Promise.all(resolvedArray);
     return e.parents === null ? response[0] : response;
-  }
+  };
 }
